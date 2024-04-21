337c2a8a70eb96d40693aa4c67ddc64247c3d664
Qafar <mirqafar2004@gmail.com>
2024-04-22 00:50:14 +0400

    Add project files.

---
+++ azeriChat/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace azeriChat;
+
+
+public partial class MainWindow : Window
+{
+    public int FontSize1 { get; set; } = 15;
+    public int FontSize2 { get; set; } = 25;
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        listBox.Items.Add($"{DateTime.Now}          {textBox.Text}");
+    }
+
+    private void textBox_Key(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            listBox.Items.Add($"{DateTime.Now}          {textBox.Text}");
+            textBox.Text = "";
+        }
+    }
+}
